946f1243526be943016e76a213825a962233eb56
OdaxHUN <89915053+ProcyxDEV@users.noreply.github.com>
2023-01-06 12:37:20 +0100

    Add files via upload

---
+++ esx_carthief/locales/en.lua
@@ -0,0 +1,13 @@
+Locales['en'] = {
+  ['vehicle_robbery'] = 'Autó lopás',
+  ['mission_failed'] = 'küldetés Nem sikerült!',
+  ['get_back_car_10s'] = '10 másodperc áll rendelkezésére, hogy visszatérjen az autóba',
+  ['get_back_car_1m'] = '1 perce van arra, hogy visszaüljön az autóba',
+  ['car_provided_rule'] = 'Használnia kell az Ön számára biztosított autót, és teljesen meg kell állnia.',
+  ['already_robbery'] = 'Már folyamatban van egy autórablás!',
+  ['not_enough_cops'] = 'nincs elég rendőr a városban!',
+  ['car_stealing_in_progress'] = 'autólopás folyamatban. A járműkövető aktív lesz a radaron',
+  ['steal_a_car'] = 'Nyomja meg a ~INPUT_CONTEXT~ gombot az autó ellopásához',
+  ['drop_car_off'] = 'Nyomja meg a ~ INPUT_CONTEXT ~ gombot az autó leadásához',
+  ['cooldown'] = 'Nemrég fejeződött be egy járműrablás. Várjon %s másodperceket, hogy ellopjon egy másikat.'
+}
